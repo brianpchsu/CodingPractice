1b9b50054ffc6b2ea501f8386ec329260829e5bf
brianpchsu <brianpchsu@gmail.com>
2015-05-21 10:08:09 -0700

    Add logic explanation to isPalindrome

--- isPalindrome.js
+++ isPalindrome.js
@@ -3,13 +3,18 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-  var pureStringArr = s.replace(/[^a-zA-Z0-9]/g,'').toLowerCase().split('');
-  var validLength = pureStringArr.length;
+  // replace non alphanumeric character with empty string and conver all to lower case
+  var pureString = s.replace(/[^a-zA-Z0-9]/g,'').toLowerCase();
+  // get new string's length
+  var validLength = pureString.length;
+  // check head and tail's string character
   for(var i = 0; i < Math.floor(validLength/2); i++){
-    if(pureStringArr[i] !== pureStringArr[validLength - 1 - i]){
+    // if two characters are not the same, return false
+    if(pureString[i] !== pureString[validLength - 1 - i]){
       return false;
     }
   }
+  // if all characters match, return true
   return true;
 }
 

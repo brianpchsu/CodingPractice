4f5ff19bb34809f4264879529f9b1fd94265e344
brianpchsu <brianpchsu@gmail.com>
2015-05-24 22:11:42 -0700

    Update the doc for containsDuplicate

--- containsDuplicate.js
+++ containsDuplicate.js
@@ -3,15 +3,19 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
+  // define object for record occurance
   var occurance = {};
+  // check each element in array, update occurance
   for(var i = 0; i < nums.length; i++){
     occurance[nums[i]] = occurance[nums[i]] + 1 || 1;
   }
+  // check each key in occurance, return true if occured more than once
   for(var key in occurance){
     if(occurance[key] > 1){
       return true;
     }
   }
+  // if no element occured more than once, no duplicate
   return false;
 };
 

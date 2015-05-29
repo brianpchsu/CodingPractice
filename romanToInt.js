4aa2217e89553d47b6233d1e0a31cfeefd193da4
brianpchsu <brianpchsu@gmail.com>
2015-05-28 21:31:20 -0700

    Add explanation to romanToInt

--- romanToInt.js
+++ romanToInt.js
@@ -2,27 +2,36 @@
  * @param {string} s
  * @return {number}
  */
- var DIGIT_VALUES = {
-   I: 1,
-   V: 5,
-   X: 10,
-   L: 50,
-   C: 100,
-   D: 500,
-   M: 1000
- };
+var DIGIT_VALUES = {
+  I: 1,
+  V: 5,
+  X: 10,
+  L: 50,
+  C: 100,
+  D: 500,
+  M: 1000
+};
 
-var romanToInt = function(romanNumeral) {
-  if (typeof romanNumeral !== 'string') return null; // return null if input is not string
-  var digitIndex = Object.keys(DIGIT_VALUES);
-  var input = romanNumeral.split('');
-  result = 0;
-  for (var i =0; i< input.length; i++){
-    if (digitIndex.indexOf(input[i]) < digitIndex.indexOf(input[i+1])){
+var digitIndex = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+
+var romanToInt = function(romanNumeral){
+  // define input string and final result
+  var input = romanNumeral.split(''), result = 0;
+
+  // iterate through the inpu
+  for(var i = 0; i< input.length; i++){
+    // if current digit is less than next digit, current number should be deducted
+    if(digitIndex.indexOf(input[i]) < digitIndex.indexOf(input[i+1])){
       result -= DIGIT_VALUES[input[i]];
+      // otherwise the number should be added
     } else {
       result += DIGIT_VALUES[input[i]];
     }
   }
   return result;
 };
+
+console.log(translateRomanNumeral("VI"));
+console.log(translateRomanNumeral("IV"));
+console.log(translateRomanNumeral("XVI"));
+console.log(translateRomanNumeral("LXXXIII"));

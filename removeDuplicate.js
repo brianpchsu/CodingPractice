7a6b4796ec6b1691aeb2712630e63b22cb74ad3b
brianpchsu <brianpchsu@gmail.com>
2015-05-30 18:51:44 -0700

    Add explanation to removeDuplicate

--- removeDuplicate.js
+++ removeDuplicate.js
@@ -3,14 +3,21 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
+  // if nums length is 0 or 1, return the length (no duplicate)
   if(nums.length < 2) return nums.length;
+  // define index and arrayLength
   var index = 1, arrLength = nums.length;
+  // iterate through the array
   for(var i = 1; i < arrLength; i++){
+    // if current one is different than the previous one
     if(nums[i] !== nums[i-1]){
+      // rewrite the index position element to the current i
       nums[index] = nums[i];
+      // increment the index
       index++;
     }
   }
+  // return the index (final distinct number)
   return index;
 };
 

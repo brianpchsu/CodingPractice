a0ae0fd060c232e78cca9e447b163704db71c0b5
brianpchsu <brianpchsu@gmail.com>
2015-06-03 19:24:06 -0700

    Update explanation to minDepthTree

--- Tree/minDepthTree.js
+++ Tree/minDepthTree.js
@@ -14,15 +14,20 @@ function TreeNode(val) {
     this.left = this.right = null;
 }
 
+// recursive solution
 var minDepth = function(root) {
+  // if root is null, then return 0
   if(!root){
     return 0;
   }
+  // if left node is null, check the right node branch
   if(!root.left){
     return minDepth(root.right)+1;
   }
+  // if right node is null, check the left node branch
   if(!root.right){
     return minDepth(root.left)+1;
   }
+  // return the min of two branches
   return Math.min(minDepth(root.left), minDepth(root.right))+1;
 };

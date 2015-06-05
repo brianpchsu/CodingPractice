8950a97fdededa74bd6fd9176410dd55c98c5d76
brianpchsu <brianpchsu@gmail.com>
2015-06-04 22:25:57 -0700

    Add explanation to deleteDuplicates

--- LinkedList/deleteDuplicates.js
+++ LinkedList/deleteDuplicates.js
@@ -15,14 +15,21 @@ function ListNode(val) {
 }
 
 var deleteDuplicates = function(head) {
+  // if head is null, return null
   if(!head) return null;
+  // define pointer for operation
   var pointer = head;
+  // check from the beginning of the list, ending in pointer.next === null
   while(pointer.next !== null){
+    // if the current val is equal to the next val
     if (pointer.val === pointer.next.val){
+      // remove the next with the next's next
       pointer.next = pointer.next.next;
     } else {
+      // if the next is different, then move the pointer to the next one
       pointer = pointer.next;
     }
   }
+  // return the head (starting point list)
   return head;
 };

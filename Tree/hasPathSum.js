3bba094dc4e8938f34852a95639318f0202f54b5
brianpchsu <brianpchsu@gmail.com>
2015-06-04 21:57:28 -0700

    Update explanation to hasPathSum

--- Tree/hasPathSum.js
+++ Tree/hasPathSum.js
@@ -16,11 +16,14 @@ function TreeNode(val) {
 }
 
 var hasPathSum = function(root, sum) {
+  // if original root is null, return false (impossible)
   if(root === null){
     return false;
   }
+  // if it's leaf, check if the root's val equal to current desired sum
   if(root.left === null && root.right === null){
     return sum === root.val;
   }
+  // if it's the root or middle node, use recursion to check the remaining value
   return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
 };

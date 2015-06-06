69073dd9c7ab4070cf30381a381dbd461fc61483
brianpchsu <brianpchsu@gmail.com>
2015-06-06 13:44:46 -0700

    Add explanation to maxDepthTree

--- Tree/maxDepthTree.js
+++ Tree/maxDepthTree.js
@@ -15,8 +15,11 @@ function TreeNode(val) {
 }
 
 var maxDepth = function(root) {
+  // if root is null, reach leaf, return 0 level
   if(!root) return 0;
+  // if either left or right branch is null, check the other side of branch, add this level 1 to the calculation
   if(!root.left) return maxDepth(root.right) + 1;
   if(!root.right) return maxDepth(root.left) + 1;
+  // if both branches exist, check the max of the branch
   return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
 };

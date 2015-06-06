97ebb37f34b2b7e7c83217cccd087562f4f1656b
brianpchsu <brianpchsu@gmail.com>
2015-06-06 15:50:28 -0700

    Add expanation to getPascalTriangleRow

--- Array/getPascalTriangleRow.js
+++ Array/getPascalTriangleRow.js
@@ -2,15 +2,20 @@
  * @param {number} rowIndex
  * @return {number[]}
  */
-//
+// Time complexity O(n2), time complexity 1 d array
 var getRow = function(rowIndex) {
+  // define base condition
   var result = [1];
+  // if rowIndex is 0, return base array
   if(rowIndex < 1) return result;
-
+  // iterate from 1 to rowIndex
   for(var i = 1; i <= rowIndex; i++){
+    // update array from the current result end to position 1
     for( j = result.length-2; j>=0; j--){
+      // the new j+1 is combined from old j and j+1
       result[j+1] = result[j] + result[j+1];
     }
+    // add additional 1 to the end
     result.push(1);
   }
   return result;
@@ -19,6 +24,7 @@ var getRow = function(rowIndex) {
 console.log(getRow(3));
 
 // use PascalTriangle I method to get the row info
+// time complexity O(n2), space complexity 2d array
 // var getRow = function(rowIndex) {
 //   var result = [];
 //   var first = [1];

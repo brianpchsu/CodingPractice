fd5ec9503322e3e0a76fb4f54623e01a30a7f70b
brianpchsu <brianpchsu@gmail.com>
2015-06-06 14:10:36 -0700

    Add explanation to isSymmetricTree

--- Tree/isSymmetricTree.js
+++ Tree/isSymmetricTree.js
@@ -15,13 +15,20 @@ function TreeNode(val) {
 }
 
 var isSymmetric = function(root) {
+  // if root is null, then it's symmetric
   if(!root) return true;
+  // check if left and right branch are symmetric
   return checkBranchSymetric(root.left, root.right);
 };
 
+// helper function to check the left and right branch
 var checkBranchSymetric = function(leftNode, rightNode){
+  // if both branch are null, then it's symmetric
   if (!leftNode && !rightNode) return true;
+  // if only one branch is null, then not symmetric
   if (!leftNode || !rightNode) return false;
+  // check the value of both branch
   if(leftNode.val !== rightNode.val) return false;
+  // check left and right branch's child branch
   return (checkBranchSymetric(leftNode.left, rightNode.right) && checkBranchSymetric(leftNode.right, rightNode.left));
 };

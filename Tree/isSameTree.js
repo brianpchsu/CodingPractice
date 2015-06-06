b899f7ffd66ffc4993e960370104f2c6d65b715e
brianpchsu <brianpchsu@gmail.com>
2015-06-06 10:47:15 -0700

    Add explanation to isSameTree

--- Tree/isSameTree.js
+++ Tree/isSameTree.js
@@ -16,8 +16,10 @@ function TreeNode(val) {
 }
 
 var isSameTree = function(p, q) {
+  // if both node is null, ending situation, return true
   if(!p && !q) return true;
+  // if only one node is null, not the same tree
   if(!p || !q) return false;
-
+  // compare the current node value and the left and right branch
   return  (p.val === q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };

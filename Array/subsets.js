1f152851838f7c4b3123f4cfe11b14081ab6bd17
brianpchsu <brianpchsu@gmail.com>
2015-06-08 11:51:53 -0700

    Finish subsets

---
+++ Array/subsets.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsets = function(nums) {
+  // assign results to empty set
+  var results = [[]];
+
+  // for each member in argument set
+  for (var i = 0; i < nums.length; i ++) {
+    results = results.concat(results.map(function (subset) {
+      return subset.concat(nums[i]);
+    }));
+  };
+  results.map(function(subset){
+    return subset.sort(function(a,b){
+      return a-b;
+    });
+  });
+  return results;
+};
+
+
+console.log(subsets([4,1,0]));
+// console.log(subsets([1,2,3,4,5,6,7,8,10,0]));

c89a07e206dd97e4cbd6e5b136175646e7ff0840
brianpchsu <brianpchsu@gmail.com>
2015-06-08 17:54:50 -0700

    Add explanation to addTwoNumbers

--- LinkedList/addTwoNumbers.js
+++ LinkedList/addTwoNumbers.js
@@ -16,23 +16,31 @@ function ListNode(val) {
 }
 
 var addTwoNumbers = function(l1, l2) {
+  // if one list is null, no point to add, return the other one
   if(!l1) return l2;
   if(!l2) return l1;
-
+  // define a new list for final result
   var newhead = new ListNode(-1);
+  // use helper pointer for tracking
   var pointer = newhead;
   var carry = 0;
+  // if either of the node in the list still not null, add the node to the final result
   while(l1 || l2){
+    // get value from each node
     var value1 = l1? l1.val: 0;
     var value2 = l2? l2.val: 0;
+    // create node for the sumValue
     var sumValue = Math.floor((value1 + value2 + carry)%10);
     var node = new ListNode(sumValue);
+    // record carry
     carry = Math.floor((value1 + value2 + carry)/10);
+    // update the pointer and prepare for next itertion
     pointer.next = node;
     pointer = pointer.next;
     l1 = l1? l1.next: null;
     l2 = l2? l2.next: null;
   }
+  // if after running all digit and there is still carry, add the new digit
   if(carry === 1){
     pointer.next = new ListNode(1);
   }

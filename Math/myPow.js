d0ba4f3804f380853131cac8f8781d034386f045
brianpchsu <brianpchsu@gmail.com>
2015-06-09 14:07:06 -0700

    Finish myPow

---
+++ Math/myPow.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+  if(n === 0) return 1;
+  if(n < 0) {
+    return 1 / myPow(x, -n);
+  }
+  var half = myPow(x, Math.floor(n/2));
+  if(n % 2 === 0){
+    return half * half;
+  } else {
+    return half * half * x;
+  }
+};
+
+console.log(myPow(34.00515, -3));

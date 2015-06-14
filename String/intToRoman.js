d1bb3b081321287dd61b2b55ecb4a182f480fc5e
brianpchsu <brianpchsu@gmail.com>
2015-06-13 18:44:11 -0700

    Add explanation to intToRoman

--- String/intToRoman.js
+++ String/intToRoman.js
@@ -3,10 +3,13 @@
  * @return {string}
  */
 var intToRoman = function(num) {
+  // define roman symbol and the roman value
   var symbol = ["M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"];
   var value = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  // final roman result
   var roman ='';
-  for(var i = 0; num !==0; ++i){
+  // if num is not zero, deduct the num using the couterpart value and add the value to result roman
+  for(var i = 0; num !==0; i++){
     while(num >= value[i]){
       num -= value[i];
       roman += symbol[i];
@@ -16,3 +19,7 @@ var intToRoman = function(num) {
 };
 
 console.log(intToRoman(3900));
+console.log(intToRoman(2200));
+console.log(intToRoman(999));
+console.log(intToRoman(50));
+console.log(intToRoman(6));

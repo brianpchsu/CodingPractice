b3e180f99d186f4f4f0d5ddf5953c6403b9aef2f
brianpchsu <brianpchsu@gmail.com>
2015-06-18 06:38:18 +0800

    Add explanation to preorderTraversal

--- Tree/preorderTraversal.js
+++ Tree/preorderTraversal.js
@@ -10,15 +10,23 @@
  * @return {number[]}
  */
 var preorderTraversal = function(root) {
+  // define result array and stack for tracking
   var result = [], stack = [];
+  // if root is empty, return result
   if(!root) return result;
+  // push the root to stack as beginning state
   stack.push(root);
+  // keep the while loop if there is element in the stack
   while(stack.length >0){
+    // get the last element from stack
     var top = stack.pop();
+    // record the value to result
     result.push(top.val);
+    // record right and left to stack, right first because stack is FILO
     if(top.right) stack.push(top.right);
     if(top.left) stack.push(top.left);
   }
+  // return the final val result
   return result;
 };
 

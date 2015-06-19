a44c29160b5cd163de967dcaf8fea0c4c2a4fa85
brianpchsu <brianpchsu@gmail.com>
2015-06-19 19:03:20 +0800

    Add explanation to LinkedListCycle

--- LinkedList/LinkedListCycle.js
+++ LinkedList/LinkedListCycle.js
@@ -11,14 +11,19 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
+  // if head is null, no cycle
   if (!head) return false;
+  // use two pointers for tracking, if there is a cycle, then the two
+  // pointers will meet sometime
   var fast = head, slow = head;
   while(fast && fast.next){
     slow = slow.next;
     fast = fast.next.next;
+    // if the two pointers meet, then return true
     if(slow === fast){
       return true;
     }
   }
+  // if reaching here, fast to the end node, then no cycle
   return false;
 };

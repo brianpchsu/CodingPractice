f9a8b59eabc51dc7838711c9839191c880e2393e
brianpchsu <brianpchsu@gmail.com>
2015-06-30 14:22:13 +0800

    Add explanation to threeSumClosest

--- Array/threeSumClosest.js
+++ Array/threeSumClosest.js
@@ -7,7 +7,7 @@ var threeSumClosest = function(nums, target) {
   var result = 0, minDiff = Infinity;
   // if there is less than 3 items in the array, no way to have 3Sum
   if(!nums || nums.length <3) return result;
-  // sort the array
+  // sort the array in ascending order
   nums = nums.sort(function(a,b){
     return a-b;
   });
@@ -22,19 +22,21 @@ var threeSumClosest = function(nums, target) {
         minDiff = 0;
         result = sum;
         break;
-        // if twoSum is not big enough, increment the start
+        // if sum is not matching to target, check the differences and update the result if needed
       } else if(sum < target){
        if(target - sum < minDiff){
         minDiff = target - sum;
         result = sum;
        }
+       // increment the start to the next index
        start++;
-        // if twoSum to bigger than twoSumTarget, decrement the end
+        // if sum is not matching to target, check the difference and update the result if needed
       } else {
         if(sum - target < minDiff){
           minDiff = sum - target;
           result = sum;
         }
+        // decrement the end index
         end--;
       }
     }

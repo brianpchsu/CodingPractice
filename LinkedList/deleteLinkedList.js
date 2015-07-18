495aa10d517d9b593a9b9cdb8f8d71a4d6944383
brianpchsu <brianpchsu@gmail.com>
2015-07-18 06:42:18 -0700

    Add explanation to deleteLinkedList

--- LinkedList/deleteLinkedList.js
+++ LinkedList/deleteLinkedList.js
@@ -10,6 +10,7 @@
  * @return {void} Do not return anything, modify node in-place instead.
  */
 var deleteNode = function(node) {
+  // update the node to the next node with the right value and next node
   node.val = node.next.val;
   node.next = node.next.next;
 };

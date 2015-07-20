4b9ed7070ffcf23570d6743c71654107515c6d50
brianpchsu <brianpchsu@gmail.com>
2015-07-19 19:16:25 -0700

    Add explanation to invertTree

--- Tree/invertTree.js
+++ Tree/invertTree.js
@@ -9,11 +9,14 @@
  * @param {TreeNode} root
  * @return {TreeNode}
  */
-// recursive
+// recursive way
 var invertTree = function(root) {
   if(!root) return null;
+  // save root.left for later
   var temp = root.left;
+  // root .left be update using root.right (also need to undergo inverTree for each subTree)
   root.left = invertTree(root.right);
+  // root .right be update using root.left (also need to undergo inverTree for each subTree)
   root.right = invertTree(temp);
   return root;
 };
@@ -23,21 +26,29 @@ function TreeNode(val) {
     this.left = this.right = null;
 }
 
-// iterative
+// iterative way
 var invertTree = function(root){
   if(!root) return null;
+  // using queue (array for simplification)
   var queue = [root];
+  // if there is item in the queue
   while(queue){
+    // get the first item in the queue
     var front = queue.shift();
     // console.log("front is ", front);
+    // save the item's left to queue
     if(front.left){
       queue.push(front.left);
     }
+    // save the item's right to the queue
     if(front.right){
       queue.push(front.right);
     }
+    // save front.left to temp for later
     var temp = front.left;
+    // update front.left using front.right
     front.left = front.right;
+    // update front.right using front.left (temp)
     front.right = temp;
   }
   return root;

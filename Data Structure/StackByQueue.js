fd13d038b01ed839b60d5ddd00fdbeb8950be026
brianpchsu <brianpchsu@gmail.com>
2015-07-19 20:27:04 -0700

    Add explanation to StackByQueue

--- Data Structure/StackByQueue.js
+++ Data Structure/StackByQueue.js
@@ -22,6 +22,7 @@ var Queue = function(){
 } ;
 
 var Stack = function() {
+  // use two Queue for tracking, q1 for older record, q2 for the latest input
   this.q1 = new Queue();
   this.q2 = new Queue();
 };
@@ -31,7 +32,9 @@ var Stack = function() {
  * @returns {void}
  */
 Stack.prototype.push = function(x) {
+  // save the latest input to q2
   this.q2.push(x);
+  // if q2 is greater than 1, then move the older items to q1
   while(this.q2.size() > 1){
     this.q1.push(this.q2.pop());
   }
@@ -41,7 +44,9 @@ Stack.prototype.push = function(x) {
  * @returns {void}
  */
 Stack.prototype.pop = function() {
+  // do top to make sure we have the right number in q2
   this.top();
+  // pop the q2 to get the number
   this.q2.pop();
 };
 
@@ -49,12 +54,16 @@ Stack.prototype.pop = function() {
  * @returns {number}
  */
 Stack.prototype.top = function() {
+  // if q2 is empty, then we need to get the number from q1
   if(this.q2.isEmpty()){
+    // the target number is in the end of q1, so we move the head to the end and do it q1.size()-1 times
     for(var i = 0; i < this.q1.size(); i++){
       this.q1.push(this.q1.pop());
     }
+    // the head of q1 is the latest item, move that to q2
     this.q2.push(this.q1.pop());
   }
+  // return the q2 item for peeking
   return this.q2.peek();
 };
 
@@ -62,5 +71,6 @@ Stack.prototype.top = function() {
  * @returns {boolean}
  */
 Stack.prototype.empty = function() {
+  // check empty by looking q1 and q2 together
   return this.q1.isEmpty() && this.q2.isEmpty();
 };

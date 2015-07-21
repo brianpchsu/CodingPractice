d9d988be85f1474ecc4c7c0385c7566f158cc3e6
brianpchsu <brianpchsu@gmail.com>
2015-07-20 17:28:57 -0700

    Add explanation to summaryRanges

--- Array/summaryRanges.js
+++ Array/summaryRanges.js
@@ -3,16 +3,25 @@
  * @return {string[]}
  */
 var summaryRanges = function(nums) {
+  // define i for position, size for total length, result for final output
   var i = 0, size = nums.length, result = [];
+  // while position is not over the size
   while(i < size){
+    // get current position to current, item convert from number to string
     var current = i, item = nums[i].toString();
+    // if it's still in the range, and the difference between the current and next item is 1
     while( i+1 < size && (nums[i+1] - nums[i] === 1)){
+      // increment the position
       i += 1;
     }
+    // if i is greater than the original current, there is a range
     if(i > current){
+      // get the range string
       item += "->" + nums[i].toString();
     }
+    // push the range string to result
     result.push(item);
+    // increment i to the next position
     i += 1;
   }
   return result;

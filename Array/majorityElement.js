59f20279989122d30cc52c9b2f1132606fad51b6
brianpchsu <brianpchsu@gmail.com>
2015-07-21 23:07:29 -0700

    Add second solution and explanation to majorityElement

--- Array/majorityElement.js
+++ Array/majorityElement.js
@@ -2,8 +2,11 @@
  * @param {number[]} nums
  * @return {number}
  */
+// Solution 1: using hashmap O(2n)
 var majorityElement = function(nums) {
+  // define a hashmap
   var map = {};
+  // iterate through array and record the occurance of each number
   for(var i = 0; i < nums.length; i++){
     if(map[nums[i]]){
       map[nums[i]]++;
@@ -11,6 +14,7 @@ var majorityElement = function(nums) {
       map[nums[i]] = 1;
     }
   }
+  // go over each number and return the majority key in the hashmap
   for(var key in map){
     if(map[key] > nums.length/2){
       return parseInt(key);
@@ -18,4 +22,10 @@ var majorityElement = function(nums) {
   }
 };
 
+// Solution 2: since majority element occur more than n/2 times,
+// after we sort the array, the middle element will be the majority one
+var majorityElement = function(nums) {
+  return nums.sort()[Math.floor(nums.length/2)];
+};
 
+console.log(majorityElement([1, 1, 2, 4, 1]));

5c36d89133745b75a67c4c849236929654c5bc8c
brianpchsu <brianpchsu@gmail.com>
2015-07-21 22:46:21 -0700

    Add explanation to titleToNumber

--- Math/titleToNumber.js
+++ Math/titleToNumber.js
@@ -3,8 +3,12 @@
  * @return {number}
  */
 var titleToNumber = function(s) {
+  // define result 0 for start
   var result = 0;
+  // go over each digit
   for(var i = 0; i < s.length; i++){
+    // the result will be the current one (check the character code) - character code A (65) + 1
+    // and add the previous one times 26
     result = result * 26 + (s[i].charCodeAt(0) - 64);
   }
   return result;

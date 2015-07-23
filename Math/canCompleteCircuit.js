fcfbee83cee051ea9da614ce86735889786900de
brianpchsu <brianpchsu@gmail.com>
2015-07-22 21:18:44 -0700

    Add explanation to canCompleteCircuit

--- Math/canCompleteCircuit.js
+++ Math/canCompleteCircuit.js
@@ -4,18 +4,22 @@
  * @return {number}
  */
 var canCompleteCircuit = function(gas, cost) {
-  var diff = new Array(gas.length);
+  // define remainingGas for overall record, currentSum for checking current valid station,
+  // startNode for the potential final output position
+  var remainingGas = 0, currentSum = 0, startNode = 0;
   for(var i = 0; i < gas.length; i++){
-    diff[i] = gas[i] - cost[i];
-  }
-  var leftGas = 0, sum = 0, startnode = 0;
-  for(var i = 0; i < gas.length; i++){
-    leftGas += diff[i];
-    sum += diff[i];
-    if(sum < 0){
-      startnode = i+1;
-      sum = 0;
+    // update the remainingGas with each station's adding gas and comcurrentSumption
+    remainingGas += gas[i] - cost[i];
+    // check the currentSum for that station
+    currentSum += gas[i] - cost[i];
+    // if the currentSum is less than 0, then it's not possible to circle from that point (run out of gas)
+    // update the node to the next one and currentSum to 0
+    if(currentSum < 0){
+      startNode = i+1;
+      currentSum = 0;
     }
   }
-  return leftGas < 0? -1: startnode;
+  // if overall remainingGas is less than 0, then it's not possible to go over circular route
+  // otherwise it's possible to start from the startNode position
+  return remainingGas < 0? -1: startNode;
 };

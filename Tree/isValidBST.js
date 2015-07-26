25eb685cd606414e6de526f56fa3a0c1d4998847
brianpchsu <brianpchsu@gmail.com>
2015-07-26 09:48:37 -0700

    Add explanation to isValidBST

--- Tree/isValidBST.js
+++ Tree/isValidBST.js
@@ -10,11 +10,15 @@
  * @return {boolean}
  */
 var isValidBST = function(root, min, max) {
+  // if root is null, then return true
   if (!root) return true;
+  // get minimun and maxmium value from input or default (Infinity and -Infinity)
   var minValue = min || -Infinity, maxValue = max || Infinity;
-  if(root.val <= min || root.val >= max){
+  // if current root value is less than minValue or greater than maxValue, not a valid BST
+  if(root.val <= minValue || root.val >= maxValue){
     return false;
   }
+  // check the left and right branch using the current root value as new boundry
   return isValidBST(root.left, minValue, root.val) && isValidBST(root.right, root.val, maxValue);
 };
 

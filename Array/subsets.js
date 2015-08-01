672a1998ab8d6e6a9cc75f66e3fafada545bcbed
brianpchsu <brianpchsu@gmail.com>
2015-08-01 16:13:19 -0700

    Add solution to subsets

--- Array/subsets.js
+++ Array/subsets.js
@@ -2,6 +2,7 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
+// Solution 1
 var subsets = function(nums) {
   // assign results to empty set
   var results = [[]];
@@ -22,6 +23,48 @@ var subsets = function(nums) {
   return results;
 };
 
+// Solution 2
+var subsets = function(nums){
+  var result = [[]];
+  if(!nums || nums.length ===0) return result;
+  nums.sort(function(a,b){
+    return a-b;
+  });
+  for(var i = 0; i < nums.length; i++){
+    var size = result.length;
+    for(var j = 0; j < size; j++){
+      var item = result[j];
+      item = item.concat(nums[i]);
+      result.push(item);
+    }
+  }
+  return result;
+};
 
+// // Solution 3
+// var subsets = function(nums){
+//   if(nums === null) return null;
+//   nums.sort(function(a, b){
+//     return a-b;
+//   });
+//   return helper(nums, nums.length-1);
+// };
+
+// var helper = function(nums, index){
+//   if(index === -1){
+//     var result = [];
+//     var elem = [];
+//     result.push(elem);
+//     return result;
+//   }
+//   result = helper(nums, index-1);
+//   var size = result.length;
+//   for(var i = 0; i < size; i++){
+//     var elem = result[i];
+//     elem.push(nums[i]);
+//     result.push(elem);
+//   }
+//   return result;
+// }
 console.log(subsets([4,1,0]));
 // console.log(subsets([1,2,3,4,5,6,7,8,10,0]));

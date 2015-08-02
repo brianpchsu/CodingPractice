3ca9fbc84ff345e281878c5bb479f601981a155d
brianpchsu <brianpchsu@gmail.com>
2015-08-02 16:54:36 -0700

    Add iterative solution to permute

--- Backtracking/permute.js
+++ Backtracking/permute.js
@@ -3,30 +3,7 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-// var permute = function(nums) {
-//   if(nums.length === 1) return nums;
-//   result = [[nums[0]]];
-
-//   for(var i = 1; i < nums.length; i++){
-//     var resultLength = result.length;
-//     for(var j = 0; j < resultLength; j++){
-//       // console.log("resultLength", resultLength);
-//       var temp = result[j].slice();
-//       dele
-//       for(var k = 0; k <= temp.length; k++){
-//         console.log("result is ", result);
-//         console.log("temp is ", temp);
-//         var temp2 = temp.splice(k, 0, nums[i]);
-//         console.log("temp2 is ", temp2);
-//         result.push(temp2);
-//         console.log("result", result);
-//       }
-//       // result[j].push(nums[i]);
-//     }
-//   }
-//   return result;
-// };
-
+// Solution 1 recursive
 var helper = function(nums, used, item, result){
   if(item.length === nums.length){
     // console.log("push item", item);
@@ -46,13 +23,36 @@ var helper = function(nums, used, item, result){
   }
 };
 
+// var permute = function(nums) {
+//   if(nums.length <= 1 ) return [nums];
+//   var result = [];
+//   helper(nums, [], [], result);
+//   return result;
+// };
+
+// Solution 2 iterative
 var permute = function(nums) {
-  if(nums.length <= 1 ) return [nums];
-  var result = [];
-  helper(nums, [], [], result);
+  if(nums.length <= 1) return [nums];
+  result = [[nums[0]]];
+
+  for(var i = 1; i < nums.length; i++){
+    var newResult = [];
+    for(var j = 0; j < result.length; j++){
+      var current = result[j];
+      for(var k = 0; k <= current.length; k++){
+        console.log("var current", current);
+        var item = current.slice();
+        item.splice(k, 0, nums[i]);
+        console.log("item", item);
+        newResult.push(item.slice());
+        console.log("newResult", newResult);
+      }
+    }
+    result = newResult;
+    console.log("result", result);
+  }
   return result;
 };
-
-
-console.log(permute([1]));
+// console.log(permute([1]));
 console.log(permute([1,2]));
+// console.log(permute([1,2,3]));

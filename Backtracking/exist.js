9698d4b97c64b68f1a5a2e9f7df5cdd78320b6e7
brianpchsu <brianpchsu@gmail.com>
2015-08-09 11:32:56 -0700

    WIP: exist Word Search

---
+++ Backtracking/exist.js
@@ -0,0 +1,35 @@
+/**
+ * @param {character[][]} board
+ * @param {string} word
+ * @return {boolean}
+ */
+var exist = function(board, word) {
+  // if(board.length === 0 || board[0].length === 0) return false;
+  var visited = board.slice();
+  for(var i = 0; i < board; i++){
+    for(var j = 0; j < board[0].length; j++){
+      if(findWord(board, visited, i, j, word, 0)){
+        return true;
+      }
+    }
+  }
+  return false;
+};
+
+var findWord = function(board, visited, row, col, word, index){
+  if(index === word.length) return true;
+    console.log("hihi", visited);
+  if(row < 0 || col < 0 || row >= board.length || col >= board[0].length || visited[i][j] || board[i][j] !== word[index]){
+    return false;
+  }
+  visited[i][j] = true;
+  console.log("visited", visited);
+  if(findWord(board, visited, row-1, col, word, index+1)) return true;
+  if(findWord(board, visited, row+1, col, word, index+1)) return true;
+  if(findWord(board, visited, row, col-1, word, index+1)) return true;
+  if(findWord(board, visited, row, col+1, word, index+1)) return true;
+  visited[row][col] = false;
+  return false;
+};
+
+console.log(exist(["a"], "a"));

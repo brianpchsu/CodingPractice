8dec00fb3973bce7a8138a06167e0f7ae278b4f7
brianpchsu <brianpchsu@gmail.com>
2015-08-10 23:53:23 -0700

    WIP: still debugging exist

--- Backtracking/exist.js
+++ Backtracking/exist.js
@@ -6,7 +6,14 @@
 var exist = function(board, word) {
   // if(board.length === 0 || board[0].length === 0) return false;
   var visited = board.slice();
-  for(var i = 0; i < board; i++){
+  for(var i = 0; i < board.length; i++){
+    for(var j = 0; j < board[0].length; j++){
+      visited[i][j] = false;
+    }
+  }
+  console.log("visited", visited);
+
+  for(var i = 0; i < board.length; i++){
     for(var j = 0; j < board[0].length; j++){
       if(findWord(board, visited, i, j, word, 0)){
         return true;
@@ -18,8 +25,10 @@ var exist = function(board, word) {
 
 var findWord = function(board, visited, row, col, word, index){
   if(index === word.length) return true;
-    console.log("hihi", visited);
-  if(row < 0 || col < 0 || row >= board.length || col >= board[0].length || visited[i][j] || board[i][j] !== word[index]){
+  console.log("visited", visited);
+
+  if(row < 0 || col < 0 || row >= board.length || col >= board[0].length || visited[row][col] || board[row][col] !== word[index]){
+    console.log("row", row, "- col", col, "- board[0]", board[0].length, "- visited[row][col]", visited[row][col], "- board[row][col]", board[row][col], "- word[index]", word[index]);
     return false;
   }
   visited[i][j] = true;

07c5b1eb22e5bde676f84d3712bbffcdfeecaaad
brianpchsu <brianpchsu@gmail.com>
2015-08-11 21:23:41 -0700

    Add explanation to getIntersectionNode

--- LinkedList/getIntersectionNode.js
+++ LinkedList/getIntersectionNode.js
@@ -12,17 +12,26 @@
  * @return {ListNode}
  */
 var getIntersectionNode = function(headA, headB) {
+  // use a object (hashmap) to store the first List
   var record = {};
+  // use two pointer to go over each list
   var p1 = headA, p2 = headB;
+  // iterate through each node in headA
   while(p1){
+    // record each node with the value
     record[p1.val] = true;
+    // move the the next list
     p1 = p1.next;
   }
+  // after iterating the first list, iterate the second list
   while(p2){
+    // if the second node's value is recorded in the record, it's the intersaction, return the node
     if(record[p2.val]){
       return p2;
     }
+    // otherwise move to the next node
     p2 = p2.next;
   }
+  // if nothing found, return null
   return null;
 };
